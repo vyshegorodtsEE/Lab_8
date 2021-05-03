aee92ab6fed398dfb0de79d8e2f77ee5ae2a6a0f
Elizaveta Vyshegorodtseva <ereshtnikova@mail.ru>
2021-05-03 20:10:27 +0300

    first

--- coding_style/testcmp.cpp
+++ coding_style/testcmp.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
-#include     "mycomplex.h"
+#include "mycomplex.h" ///< подключаем свой файл №3
+
 using namespace std;
-int main(){//тестирование класса комплексных чисел
- Complex A,B(-4), C(23.0, 45.9);
-cout<<A <<","<< B<<","<<C <<endl; A=B+C;
-cout<<"A=B+C,A="<< A<<endl;
 
-Complex M=B-C;
-cout<< "M = B - C, M = " << M << endl;cout<<"M*A="<<M*A<<endl;
-cout<<"M/4.45="<<M/4.45<<endl;
+int main() ///< главная функция
+{
+Complex A, B(-4), C(23.0, 45.9); ///< <Объявление комплексныъ переменных
+cout « A « "," « B « "," « C « endl; ///< Вывод A,B,C
+A = B + C; ///< Задача уравнения переменной А
+cout « "A = B + C, A = " « A « endl; ///< Вывод переменной А после сложения B и C
 
-    Complex D;
-  	cout<<"Enter complex number D = "; cin >>D;
- A+=C+D;
-cout << "D = " << D << "\nA = " << A << endl;return 0;}
+Complex M = B - C; ///< Объявление и рассчет комплексной переменной М
+cout « "M = B - C, M = " « M « endl; ///< Вывод переменной М после вычитания C из B
+cout « "M * A = " « M * A « endl; ///< Вывод произведения переменных A и М
+cout « "M / 4.45 = " « M / 4.45 « endl; ///< Вывод разности переменной М и 4,45
+
+Complex D; ///< Объявление комплексной переменной D
+cout « "Enter complex number D = "; ///< Вывод текста
+cin » D; ///< Ввод переменной D
+A += C + D; ///< Задача уравнения переменной А
+cout « "D = " « D « "\nA = " « A « endl; ///< Вывод переменных D и A
+
+return 0;
+}
